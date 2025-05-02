4caf7ef6a328fee2de7e9985b64e50f28cb5697a
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-05-02 16:33:24 +0200

    Fix logger

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,11 +1,17 @@
-import datetime
 import logging
+import os
+from datetime import datetime
 
 
 def get_logger():
+    dirpath = "sketchmap-python-logs"
+    os.makedirs(dirpath, exist_ok=True)
     id = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     logging.basicConfig(
-        filename=f"logs_{id}.log", format="%(asctime)s %(message)s", filemode="w"
+        filename=os.path.join(dirpath, f"logs_{id}.log"),
+        format="%(asctime)s %(message)s",
+        filemode="w",
+        level=logging.INFO,
     )
 
     return logging.getLogger()
