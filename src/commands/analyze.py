fb7f71f56f65aa652e16f728256a532eabcd843a
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-05-05 09:03:02 +0200

    Delete sketchmap comment

--- src/commands/analyze.py
+++ src/commands/analyze.py
@@ -4,28 +4,6 @@ from scipy.spatial.distance import pdist
 
 from src.utils.const import DEVICE
 
-"""
-! dimdist -D 1024 -P data/mad_llfs/merged_llfs/mad_combined_val.dat -maxd 100 -nbin 500 > mad_map/histo_mad_combined_val.dat
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-
-data_combined = np.loadtxt('histo_mad_combined_val.dat', skiprows=1)
-
-distances_combined = data_combined[:, 0] 
-histogram_values_combined = data_combined[:, 1]
-
-plt.plot(distances_combined, histogram_values_combined, color="red", label='combined')
-
-plt.xlabel('distance')
-plt.ylabel('prob density')
-plt.legend()
-plt.xlim(0,20)
-plt.show()
-
-"""
-
 
 def compute_distances(
     points: torch.Tensor, weights: torch.Tensor, weighted: bool = False
